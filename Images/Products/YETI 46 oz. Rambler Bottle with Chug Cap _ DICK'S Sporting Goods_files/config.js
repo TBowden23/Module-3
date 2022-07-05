030cd2ff8aab21a8018960561dea62d0597278ca
Student <student@StudentiMac-22.local>
2022-07-05 13:43:53 -0400

    finished footer and home page included more sections on the shop now page

---
+++ Images/Products/YETI 46 oz. Rambler Bottle with Chug Cap _ DICK'S Sporting Goods_files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C8JLLECO0T91FSTH1G5G").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C8JLLECO0T91FSTH1G5G").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C8JLLECO0T91FSTH1G5G").setPixelInfo({status: 0, name: "DICK\'S Pixel", advertiserID: "7067159448942428161", setupMode: 1, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("dickssportinggoods.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
