03b78b0e521dd797fafda2a00034272adf58f0b1
Student <student@StudentiMac-22.local>
2022-07-07 14:28:48 -0400

    updated products page to include popup tutorial video playing for my product. also fixed alignment of navbar, and added About us Content

--- Images/Products/YETI 46 oz. Rambler Bottle with Chug Cap _ DICK'S Sporting Goods_files/syndigo.js
+++
@@ -1,8 +0,0 @@
-(function (s, y, n, di, go) {
-    di = s.createElement(y);
-    di.type = 'text/java'+y;
-    di.async = true;
-    di.src = n + Math.floor(Date.now() / 86400000);
-    go = s.getElementsByTagName(y)[0];
-    go.parentNode.insertBefore(di,go);
-  }(document,'script', "https://content.syndigo.com/site/310b3f43-6186-4afe-866a-e80b34bf3082/tag.js?cv="));
